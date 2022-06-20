7e73d680722eccef63af3e7a935ab2a9d5847a05
Sálvio De Oliveira Santos Júnior <79759829+Juninho-salvio@users.noreply.github.com>
2022-06-20 10:29:06 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Meh)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("ola mundo")
+})
+basic.forever(function () {
+	
+})
